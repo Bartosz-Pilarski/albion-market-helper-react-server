f1a53a25ac1aef414762ac65612c77d255e1dbaf
Bartosz-Pilarski <bpilarski2003@protonmail.com>
2024-04-16 23:40:00 +0200

    API Refactor

--- controllers/resources.js
+++ controllers/resources.js
@@ -2,24 +2,47 @@ const resourcesRouter = require('express').Router()
 
 const Resource = require('../models/resource.js')
 
-resourcesRouter.get('/:type/:tier', async (req, res) => {
-  const requestedResource = await Resource.findOne({ tier: req.params.tier, type: req.params.type.toUpperCase() })
-
-  if(requestedResource) return res.status(200).json(requestedResource)
-  res.status(404).json({ error: 'resource not found' })
-})
-
-resourcesRouter.get('/:type', async (req, res) => {
-  const requestedResources = await Resource.find({ type: req.params.type.toUpperCase() })
-
-  if(requestedResources[0]) return res.status(200).json(requestedResources)
-  res.status(404).json({ error: 'resources not found' })
-})
+const types = {
+  METAL: ["ORE", "METALBAR"]
+}
 
 resourcesRouter.get('/', async (req, res) => {
   const requestedResources = await Resource.find({})
 
   res.status(200).json(requestedResources)
+})
+
+resourcesRouter.get('/:type', async (req, res) => {
+  const requestedType = req.params.type.toUpperCase()
+  if(!Object.hasOwn(types, requestedType)) return res.status(400).json({ error: 'incorrect parameters' })
+
+  const requestedResourcesRaw = await Resource.find({ type: types[requestedType][0] })
+  const requestedResourcesRefined = await Resource.find({ type: types[requestedType][1] })
+
+  const requestedResources = {
+    raw: requestedResourcesRaw,
+    refined: requestedResourcesRefined
+  }
+
+  if(requestedResources.raw[0] && requestedResources.refined[0]) return res.status(200).json(requestedResources)
+  res.status(404).json({ error: 'resources not found' })
+})
+
+resourcesRouter.get('/:type/:tier', async (req, res) => {
+  const requestedType = req.params.type.toUpperCase()
+  const requestedTier = parseInt(req.params.tier)
+  if(!Object.hasOwn(types, requestedType) || !(1 < requestedTier*1 < 9)) return res.status(400).json({ error: 'incorrect parameters' })
+
+  const requestedResourceRaw = await Resource.findOne({ tier: requestedTier, type: types[requestedType][0] })
+  const requestedResourceRefined = await Resource.findOne({ tier: requestedTier, type: types[requestedType][1] })
+
+  const requestedResource = {
+    raw: requestedResourceRaw,
+    refined: requestedResourceRefined
+  }
+
+  if(requestedResource) return res.status(200).json(requestedResource)
+  res.status(404).json({ error: 'resource not found' })
 })
 
 module.exports = resourcesRouter
--- services/externalDataService.js
+++ services/externalDataService.js
@@ -8,6 +8,15 @@ const fetchPricesByType = async (type) => {
   const prices = await axios.get(`${baseUrl}/prices/T1_${type},T2_${type},T3_${type},T4_${type},T5_${type},T6_${type},T7_${type},T8_${type}?locations=Caerleon,Bridgewatch,Martlock,Lymhurst,Thetford,FortSterling`)
   return Object.groupBy(prices.data, ({ item_id }) => item_id)
 }
+const fetchAll = async () => {
+  const prices = await axios.get(
+`${baseUrl}/prices/
+T2_ORE,T3_ORE,T4_ORE,T5_ORE,T6_ORE,T7_ORE,T8_ORE,
+T2_METALBAR,T3_METALBAR,T4_METALBAR,T5_METALBAR,T6_METALBAR,T7_METALBAR,T8_METALBAR
+?locations=Caerleon,Bridgewatch,Martlock,Lymhurst,Thetford,FortSterling`
+  )
+  return Object.groupBy(prices.data, ({ item_id }) => item_id)
+}
 
 const saveItemPrices = async (prices) => {
   const itemName = prices[0].item_id
@@ -34,5 +43,6 @@ const saveItemPrices = async (prices) => {
 
 module.exports = {
   fetchPricesByType,
+  fetchAll,
   saveItemPrices
 }
