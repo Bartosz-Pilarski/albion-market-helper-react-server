2b859adb8d98f828f88827f0bbbdd71d2188ba7d
Bartosz-Pilarski <bpilarski2003@protonmail.com>
2024-05-20 15:09:03 +0200

    Added epoch date to API

--- controllers/lastUpdate.js
+++ controllers/lastUpdate.js
@@ -5,6 +5,7 @@ const Resource = require('../models/resource.js')
 lastUpdateRouter.get('/', async (req, res) => {
   const dummyResourceTimestamp = await Resource.findOne({})
   const updateDate = new Date(dummyResourceTimestamp.updatedAt)
+  const epochDate = Date.parse(updateDate)
   let timeDifference = Math.floor(Date.now() - updateDate)/1000
   
   const secondsDifference = Math.floor(timeDifference % 60)
@@ -14,7 +15,7 @@ lastUpdateRouter.get('/', async (req, res) => {
   const hoursDifference = timeDifference%24
   timeDifference = Math.floor(timeDifference / 24)
 
-  res.status(200).json({ lastUpdate: updateDate, humanReadable: `${hoursDifference}h ${minutesDifference}m ${secondsDifference}s` })
+  res.status(200).json({ lastUpdate: updateDate, epoch: epochDate, humanReadable: `${hoursDifference}h ${minutesDifference}m ${secondsDifference}s` })
 })
 
 module.exports = lastUpdateRouter
